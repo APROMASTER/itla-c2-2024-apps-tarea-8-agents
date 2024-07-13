bebf1434e0a13b8df4136d9473c4de2aea7478d0
DoubleADev <arielnunezmendez@gmail.com>
2024-07-13 12:01:43 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'itla-c2-2024-apps-tarea-8-agents',
+  webDir: 'dist'
+};
+
+export default config;
